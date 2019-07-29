caba0d9ee98bee82b89ead23402eb90851fd5440
Bradley Wood <woodbr@earlham.edu>
2019-07-29 16:32:11 -0400

    minor updates

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # ec-alerts
+
+#Installation
+
+
+Place this code in your layout.html file where you'd like the alerts to be displayed.
+```
+ {{ apos.singleton(data.global, 'ecAlerts', 'ec-alerts', {currentPage: data.page._url, edit: false}) }}
+```
--- index.js
+++ index.js
@@ -5,9 +5,10 @@ module.exports = {
     modules: ['ec-alerts-global', 'ec-alerts', 'ec-alerts-widgets'],
     directory: 'lib/modules'
   },
+  permissions: 'admin',
 
   construct: function(self, options) {
     self.play = function($widget, data, options) {};
-    self.pushAsset('stylesheet', 'alert', 'always');
+    
   }
 };
