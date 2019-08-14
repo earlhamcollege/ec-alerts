811b2174b5bbb04bf243d67f1bef172951335b74
Bradley Wood <woodbr@earlham.edu>
2019-08-14 16:42:36 -0400

    fix naming conflict

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ module.exports = {
   name: 'ec-alerts',
   alias: 'ec-alerts',
   moogBundle: {
-    modules: ['ec-alerts-global', 'ec-alerts', 'ec-alerts-widgets'],
+    modules: ['ec-alerts-global', 'ec-alerts-piece', 'ec-alerts-widgets'],
     directory: 'lib/modules'
   },
   construct: function(self, options) {
